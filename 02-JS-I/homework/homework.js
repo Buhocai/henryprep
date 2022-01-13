20854e2f63a7393ea0ed1954a61e9178ce783322
BUHOCAI <ivanrussano@gmail.om>
2022-01-13 16:03:26 -0300

    finish homework 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -241,9 +241,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
-}
+if(letra.length > 1){
+    return "Dato incorrecto"
+  }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
 
+}
 
 
 // No modificar nada debajo de esta línea
